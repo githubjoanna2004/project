45460be5bf815ee30f766a86f29b12f64e6b9148
githubjoanna2004 <joannamarynebu@gmail.com>
2025-03-18 23:15:25 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module adder_multiplier(
+ module tt_um_addermultiplier(
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
