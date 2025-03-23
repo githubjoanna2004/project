b69133f2cc7bea2e747d4f50a691328f19eede98
githubjoanna2004 <joannamarynebu@gmail.com>
2025-03-23 18:49:04 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -32,11 +32,7 @@ module tt_um_adder_multiplier(
     assign uo_out[5:0] = ui_in[6]? {2'b00,adder_output}:multiplier_output;
     assign uo_out[7:6]=2'b00;
 
-  // All output pins must be assigned. If not used, assign to 0.
-  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-   // assign uo_out[0] = ui_in[0] ^ ui_in[1] ^ ui_in[2];
-  //  assign uo_out[1] = (ui_in[0] & ui_in[1]) |(ui_in[0] & ui_in[2]) |(ui_in[1] & ui_in[2]);
- //   assign uo_out[7:2] = 6'b0;
+ 
   assign uio_out = 0;
   assign uio_oe  = 0;
 
